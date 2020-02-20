98f0c60aeb3b3f46b96ed24fc1fec5b7d50045de
vdruzhinin <vdruzhinin@spscommerce.com>
2020-02-20 15:18:37 +0200

    Update to use fanout exchange type

--- receive.py
+++ receive.py
@@ -1,14 +1,13 @@
 import time
 import pika
+import sys
 import pika.exceptions as exc
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
 
 
 def callback(ch, method, properties, body):
     try:
         print(f"[x] Received {body}")
+        print(properties)
         time.sleep(2)
         ch.basic_ack(delivery_tag=method.delivery_tag)
         print(f"[x] Done {body}")
@@ -16,10 +15,18 @@ def callback(ch, method, properties, body):
         print(e)
 
 
-queue_name = 'hello'
-channel.queue_declare(queue=queue_name, durable=True)
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='logs', exchange_type='fanout', durable=True)
+
+queue_name = ''.join(sys.argv[1:])
+print(queue_name)
+result = channel.queue_declare(queue=queue_name, durable=True)
+channel.queue_bind(exchange='logs', queue=queue_name)
+
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue=queue_name, on_message_callback=callback)
+
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
 
--- send.py
+++ send.py
@@ -4,12 +4,11 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 try:
-    queue_name = 'hello'
-    channel.queue_declare(queue=queue_name, durable=True)
+    channel.exchange_declare(exchange='logs', exchange_type='fanout', durable=True)
     for m in list(range(1, 21)):
         channel.basic_publish(
-            exchange='',
-            routing_key=queue_name,
+            exchange='logs',
+            routing_key='',
             body=str(m),
             properties=pika.BasicProperties(
                 delivery_mode=2,  # make message persistent
